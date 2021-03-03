9d0b0fc60ebdb7945789d1f58bf529eceabe7c69
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2021-03-03 12:58:19 +0300

    check

--- core/settings/production.py
+++ core/settings/production.py
@@ -108,10 +108,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
