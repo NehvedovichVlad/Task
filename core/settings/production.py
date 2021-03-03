9a804437d15d91c30558dcd6ed3cbe0603ea1442
Uladzislau Nekhviadovich <vladislavnehvedovich@gmail.com>
2021-03-03 11:08:33 +0300

    check

--- core/settings/production.py
+++ core/settings/production.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.static',
     #------------------------------
     'rest_framework',
     'djoser',
@@ -110,7 +110,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 REST_FRAMEWORK = {
